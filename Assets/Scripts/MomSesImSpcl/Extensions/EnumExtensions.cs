f29163b96555042d251c54e112c5fb4b74cbc786
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-01-14 10:51:13 +0100

    Add files via upload

---
+++ Assets/Scripts/MomSesImSpcl/Extensions/EnumExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace MomSesImSpcl.Extensions
+{
+    /// <summary>
+    /// Contains extension methods for <see cref="Enum"/>.
+    /// </summary>
+    public static class EnumExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Converts an <see cref="Enum"/> value ot its underlying <see cref="int"/> representation.
+        /// </summary>
+        /// <param name="_EnumValue">The value to convert.</param>
+        /// <typeparam name="E">Must be an <see cref="Enum"/>.</typeparam>
+        /// <returns>The <see cref="int"/> representation of the <see cref="Enum"/> value.</returns>
+        public static int ToInt<E>(this E _EnumValue) where E : Enum
+        {
+            return Convert.ToInt32(_EnumValue);
+        }
+        #endregion
+    }
+}
