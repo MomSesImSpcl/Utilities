0937b94128b883ed25c4d09a3984c6d7bb3bfbb2
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-01-29 22:46:21 +0100

    Update RpcTargetExtensions.cs

--- Assets/Scripts/MomSesImSpcl/Extensions/NGO/RpcTargetExtensions.cs
+++ Assets/Scripts/MomSesImSpcl/Extensions/NGO/RpcTargetExtensions.cs
@@ -1,25 +1,25 @@
-using Unity.Netcode;
-
-namespace MomSesImSpcl.Extensions.NGO
-{
-    /// <summary>
-    /// Contains extension methods for <see cref="Unity.Netcode.RpcTarget"/>.
-    /// </summary>
-    public static class RpcTargetExtensions
-    {
-        #region Methods
-        /// <summary>
-        /// Excludes the <see cref="NetworkBehaviour.OwnerClientId"/> of the server <c>(0)</c>, and the <see cref="NetworkBehaviour.OwnerClientId"/> of the sender of the given <see cref="RpcParams"/>.
-        /// </summary>
-        /// <param name="_RpcTarget"><see cref="RpcTarget"/>.</param>
-        /// <param name="_RPCParams">The <see cref="RpcParams"/> of the sender, whose <see cref="NetworkBehaviour.OwnerClientId"/> to exclude.</param>
-        /// <returns>A <see cref="BaseRpcTarget"/> from <see cref="RpcTarget.Not(ulong[],RpcTargetUse)"/>, with the excluded <see cref="NetworkBehaviour.OwnerClientId"/>s.</returns>
-        public static BaseRpcTarget ExcludeServerAndSender(this RpcTarget _RpcTarget, RpcParams _RPCParams)
-        {
-            var _excludedOwnerClientIds = new ulong[] { 0, _RPCParams.Receive.SenderClientId };
-
-            return _RpcTarget.Not(_excludedOwnerClientIds, RpcTargetUse.Temp);
-        }
-        #endregion
-    }
-}
+// using Unity.Netcode;
+// // Can be uncommented when an Assembly Definition file is referenced, that includes the Unity.Netcode namespace.
+// namespace MomSesImSpcl.Extensions.NGO
+// {
+//     /// <summary>
+//     /// Contains extension methods for <see cref="Unity.Netcode.RpcTarget"/>.
+//     /// </summary>
+//     public static class RpcTargetExtensions
+//     {
+//         #region Methods
+//         /// <summary>
+//         /// Excludes the <see cref="NetworkBehaviour.OwnerClientId"/> of the server <c>(0)</c>, and the <see cref="NetworkBehaviour.OwnerClientId"/> of the sender of the given <see cref="RpcParams"/>.
+//         /// </summary>
+//         /// <param name="_RpcTarget"><see cref="RpcTarget"/>.</param>
+//         /// <param name="_RPCParams">The <see cref="RpcParams"/> of the sender, whose <see cref="NetworkBehaviour.OwnerClientId"/> to exclude.</param>
+//         /// <returns>A <see cref="BaseRpcTarget"/> from <see cref="RpcTarget.Not(ulong[],RpcTargetUse)"/>, with the excluded <see cref="NetworkBehaviour.OwnerClientId"/>s.</returns>
+//         public static BaseRpcTarget ExcludeServerAndSender(this RpcTarget _RpcTarget, RpcParams _RPCParams)
+//         {
+//             var _excludedOwnerClientIds = new ulong[] { 0, _RPCParams.Receive.SenderClientId };
+//
+//             return _RpcTarget.Not(_excludedOwnerClientIds, RpcTargetUse.Temp);
+//         }
+//         #endregion
+//     }
+// }
