44e1a611b4c2a7af636e3a76d802d8ca8815b45b
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-02-12 16:24:00 +0100

    Add files via upload

---
+++ Assets/Scripts/MomSesImSpcl/Data/CustomEditors/CustomEventTriggerEditor.cs
@@ -0,0 +1,40 @@
+using MomSesImSpcl.Data;
+using UnityEditor;
+using UnityEditor.EventSystems;
+
+namespace MomSesImSpcl.Editor.CustomEditors
+{
+    /// <summary>
+    /// <see cref="CustomEditor"/> for <see cref="CustomEventTrigger"/>.
+    /// </summary>
+    [CustomEditor(typeof(CustomEventTrigger))]
+    public class CustomEventTriggerEditor : EventTriggerEditor
+    {
+        #region Fields
+        /// <summary>
+        /// <see cref="CustomEventTrigger.flag"/>.
+        /// </summary>
+        private SerializedProperty flagField;
+        #endregion
+        
+        #region Methods
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            
+            this.flagField = base.serializedObject.FindProperty(CustomEventTrigger.FLAG);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.serializedObject.Update();
+            
+            EditorGUILayout.PropertyField(this.flagField);
+            
+            base.OnInspectorGUI();
+            
+            base.serializedObject.ApplyModifiedProperties();
+        }
+        #endregion
+    }
+}
