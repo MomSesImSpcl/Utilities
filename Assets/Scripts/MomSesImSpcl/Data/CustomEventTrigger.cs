93bf33e8fcb145d9ef7e04118981e25b19ebbb73
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-02-12 18:07:05 +0100

    Update CustomEventTrigger.cs

--- Assets/Scripts/MomSesImSpcl/Data/CustomEventTrigger.cs
+++ Assets/Scripts/MomSesImSpcl/Data/CustomEventTrigger.cs
@@ -1,22 +1,23 @@
+using MomSesImSpcl.Interfaces;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace MomSesImSpcl.Data
 {
     /// <summary>
-    /// <see cref="EventTrigger"/> with an optional <see cref="flag"/> field.
+    /// <see cref="EventTrigger"/> with an optional <see cref="Flag"/> field.
     /// </summary>
     public class CustomEventTrigger : EventTrigger
     {
-        #region Inspector Fields
-        [Tooltip("Optional flag to differentiate this EventTrigger from others on the same GameObject.")]
-        [SerializeField] private string flag;
-        #endregion
-        
+         #region Inspector Fields
+         [Tooltip("Optional flag to differentiate this EventTrigger from others on the same GameObject.\nMust be a class that implements the IFlag interface.")]
+         [SerializeField] private InterfaceReference<IFlag> flag;
+         #endregion
+         
 #if UNITY_EDITOR
         #region Constants
         /// <summary>
-        /// Refactor resistant field name for <see cref="flag"/>.
+        /// Refactor resistant field name for <see cref="Flag"/>.
         /// </summary>
         public const string FLAG = nameof(flag);
         #endregion
@@ -25,7 +26,7 @@ namespace MomSesImSpcl.Data
         /// <summary>
         /// <see cref="flag"/>.
         /// </summary>
-        public string Flag => this.flag;
+        public string Flag => flag?.Interface?.Flag;
         #endregion
     }
 }
