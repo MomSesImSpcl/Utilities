e84164d5cc80430923aef126a4ba8f17575fcb30
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-02-15 21:53:31 +0100

    Add files via upload

---
+++ Assets/Scripts/MomSesImSpcl/Extensions/ObscuredIntExtensions.cs
@@ -0,0 +1,28 @@
+#if ACTK_IS_HERE
+using System;
+using System.Runtime.CompilerServices;
+using CodeStage.AntiCheat.ObscuredTypes;
+
+namespace MomSesImSpcl.Extensions
+{
+    /// <summary>
+    /// Contains extension methods for <see cref="ObscuredInt"/>
+    /// </summary>
+    public static class ObscuredIntExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Casts this <see cref="ObscuredInt"/> to the given <see cref="Enum"/> <see cref="Type"/> <c>T</c>.
+        /// </summary>
+        /// <param name="_ObscuredInt">The <see cref="ObscuredInt"/> value to cast to its <see cref="Enum"/> representation.</param>
+        /// <typeparam name="T">Must be an <see cref="Enum"/>.</typeparam>
+        /// <returns>This <see cref="ObscuredInt"/> as the value inside the given <see cref="Enum"/> <see cref="Type"/>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T ToEnum<T>(this ObscuredInt _ObscuredInt)  where T : Enum
+        {
+            return Unsafe.As<ObscuredInt,T>(ref _ObscuredInt);
+        }
+        #endregion
+    }
+}
+#endif
