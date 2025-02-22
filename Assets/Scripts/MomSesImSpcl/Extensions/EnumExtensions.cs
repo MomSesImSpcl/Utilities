c1d19769ff5d4cfc3fcb3e98bf9cd8ba0f9bf441
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-02-22 14:54:45 +0100

    Update EnumExtensions.cs

--- Assets/Scripts/MomSesImSpcl/Extensions/EnumExtensions.cs
+++ Assets/Scripts/MomSesImSpcl/Extensions/EnumExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Runtime.CompilerServices;
+using Sirenix.Utilities;
 
 namespace MomSesImSpcl.Extensions
 {
@@ -18,7 +20,63 @@ namespace MomSesImSpcl.Extensions
         {
             return Enum.GetName(typeof(E), _EnumValue);
         }
-            
+        
+        /// <summary>
+        /// Converts an <see cref="Enum"/> value ot its underlying <see cref="sbyte"/> representation.
+        /// </summary>
+        /// <param name="_EnumValue">The value to convert.</param>
+        /// <typeparam name="E">Must be an <see cref="Enum"/>.</typeparam>
+        /// <returns>The <see cref="sbyte"/> representation of the <see cref="Enum"/> value.</returns>
+        public static sbyte ToSByte<E>(this E _EnumValue) where E : Enum
+        {
+#if UNITY_EDITOR
+            EnumTypeCheck<E,sbyte>();
+#endif
+            return Unsafe.As<E,sbyte>(ref _EnumValue);
+        }
+        
+        /// <summary>
+        /// Converts an <see cref="Enum"/> value ot its underlying <see cref="byte"/> representation.
+        /// </summary>
+        /// <param name="_EnumValue">The value to convert.</param>
+        /// <typeparam name="E">Must be an <see cref="Enum"/>.</typeparam>
+        /// <returns>The <see cref="byte"/> representation of the <see cref="Enum"/> value.</returns>
+        public static byte ToByte<E>(this E _EnumValue) where E : Enum
+        {
+#if UNITY_EDITOR
+            EnumTypeCheck<E,byte>();
+#endif
+            return Unsafe.As<E,byte>(ref _EnumValue);
+        }
+        
+        /// <summary>
+        /// Converts an <see cref="Enum"/> value ot its underlying <see cref="short"/> representation.
+        /// </summary>
+        /// <param name="_EnumValue">The value to convert.</param>
+        /// <typeparam name="E">Must be an <see cref="Enum"/>.</typeparam>
+        /// <returns>The <see cref="short"/> representation of the <see cref="Enum"/> value.</returns>
+        public static short ToShort<E>(this E _EnumValue) where E : Enum
+        {
+#if UNITY_EDITOR
+            EnumTypeCheck<E,short>();
+#endif
+            return Unsafe.As<E,short>(ref _EnumValue);
+        }
+        
+        /// <summary>
+        /// Converts an <see cref="Enum"/> value ot its underlying <see cref="ushort"/> representation.
+        /// </summary>
+        /// <param name="_EnumValue">The value to convert.</param>
+        /// <typeparam name="E">Must be an <see cref="Enum"/>.</typeparam>
+        /// <returns>The <see cref="ushort"/> representation of the <see cref="Enum"/> value.</returns>
+        public static ushort ToUShort<E>(this E _EnumValue) where E : Enum
+        {
+#if UNITY_EDITOR
+            EnumTypeCheck<E,ushort>();
+#endif
+            return Unsafe.As<E,ushort>(ref _EnumValue);
+        }
+        
         /// <summary>
         /// Converts an <see cref="Enum"/> value ot its underlying <see cref="int"/> representation.
         /// </summary>
@@ -27,8 +85,76 @@ namespace MomSesImSpcl.Extensions
         /// <returns>The <see cref="int"/> representation of the <see cref="Enum"/> value.</returns>
         public static int ToInt<E>(this E _EnumValue) where E : Enum
         {
-            return (int)(object)_EnumValue;
+#if UNITY_EDITOR
+            EnumTypeCheck<E,int>();
+#endif
+            return Unsafe.As<E,int>(ref _EnumValue);
         }
+        
+        /// <summary>
+        /// Converts an <see cref="Enum"/> value ot its underlying <see cref="uint"/> representation.
+        /// </summary>
+        /// <param name="_EnumValue">The value to convert.</param>
+        /// <typeparam name="E">Must be an <see cref="Enum"/>.</typeparam>
+        /// <returns>The <see cref="uint"/> representation of the <see cref="Enum"/> value.</returns>
+        public static uint ToUInt<E>(this E _EnumValue) where E : Enum
+        {
+#if UNITY_EDITOR
+            EnumTypeCheck<E,uint>();
+#endif
+            return Unsafe.As<E,uint>(ref _EnumValue);
+        }
+        
+        /// <summary>
+        /// Converts an <see cref="Enum"/> value ot its underlying <see cref="long"/> representation.
+        /// </summary>
+        /// <param name="_EnumValue">The value to convert.</param>
+        /// <typeparam name="E">Must be an <see cref="Enum"/>.</typeparam>
+        /// <returns>The <see cref="long"/> representation of the <see cref="Enum"/> value.</returns>
+        public static long ToLong<E>(this E _EnumValue) where E : Enum
+        {
+#if UNITY_EDITOR
+            EnumTypeCheck<E,long>();
+#endif
+            return Unsafe.As<E,long>(ref _EnumValue);
+        }
+        
+        /// <summary>
+        /// Converts an <see cref="Enum"/> value ot its underlying <see cref="ulong"/> representation.
+        /// </summary>
+        /// <param name="_EnumValue">The value to convert.</param>
+        /// <typeparam name="E">Must be an <see cref="Enum"/>.</typeparam>
+        /// <returns>The <see cref="ulong"/> representation of the <see cref="Enum"/> value.</returns>
+        public static ulong ToULong<E>(this E _EnumValue) where E : Enum
+        {
+#if UNITY_EDITOR
+            EnumTypeCheck<E,ulong>();
+#endif
+            return Unsafe.As<E,ulong>(ref _EnumValue);
+        }
+        
+#if UNITY_EDITOR
+        /// <summary>
+        /// Checks if the <see cref="Enum"/> <see cref="Type"/> <c>E</c> can be converted into the given <see cref="Type"/> <c>T</c>.
+        /// </summary>
+        /// <typeparam name="E">The <see cref="Type"/> of the <see cref="Enum"/>.</typeparam>
+        /// <typeparam name="T">The target <see cref="Type"/> to convert the <see cref="Enum"/> value into.</typeparam>
+        /// <exception cref="ArgumentException">When the underlying <see cref="Type"/> of the <see cref="Enum"/> doesn't match the target <see cref="Type"/> <c>T</c>.</exception>
+        private static void EnumTypeCheck<E,T>() where E : Enum where T : unmanaged
+        {
+            if (Unsafe.SizeOf<E>() != Unsafe.SizeOf<T>())
+            {
+#if ODIN_INSPECTOR
+                var _enumType = Enum.GetUnderlyingType(typeof(E)).GetNiceName().Bold();
+                var _targetType = typeof(T).GetNiceName().Bold();
+#else
+                var _enumType = Enum.GetUnderlyingType(typeof(E)).Name.Bold();
+                var _targetType = typeof(T).Name.Bold();
+#endif
+                throw new ArgumentException($"The Enum: [{typeof(E).Name.Bold()}] uses an [{_enumType}] as its underlying Type and can't be converted into a [{_targetType}].");
+            }
+        }
+#endif
         #endregion
     }
 }
