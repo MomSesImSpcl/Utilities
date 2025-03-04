679c234dc78779f58ad9c45652851275fb7c7d17
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-04 14:26:04 +0100

    Update TransformExtensions.cs

--- Assets/Scripts/MomSesImSpcl/Extensions/TransformExtensions.cs
+++ Assets/Scripts/MomSesImSpcl/Extensions/TransformExtensions.cs
@@ -1,5 +1,6 @@
 #nullable enable
 using System;
+using System.Collections.Generic;
 using MomSesImSpcl.Utilities;
 using Unity.Mathematics;
 using UnityEngine;
@@ -33,16 +34,29 @@ namespace MomSesImSpcl.Extensions
         }
         
         /// <summary>
-        /// Searched for the given <see cref="Component"/> in the children of this <see cref="Transform"/>.
+        /// Searches for the given <see cref="Component"/> in the children of this <see cref="Transform"/>.
         /// </summary>
         /// <param name="_Transform">The <see cref="Transform"/> to search under.</param>
-        /// <param name="_IncludeInactive">Whether to include inactive children in the search..</param>
+        /// <param name="_IncludeInactive">Whether to include inactive children in the search.</param>
         /// <param name="_ExcludeSelf">Set to <c>true</c> to only search for the <see cref="Component"/> in the children and not on this <see cref="Transform"/>.</param>
         /// <typeparam name="T">Must be of <see cref="Type"/> <see cref="Component"/>.</typeparam>
         /// <returns>The <see cref="Component"/> of the given <see cref="Type"/>, or <c>null</c> if it couldn't be found.</returns>
         public static T? GetComponentInChildren<T>(this Transform _Transform, bool _IncludeInactive, bool _ExcludeSelf) where T : Component
         {
             return _Transform.gameObject.GetComponentInChildren<T>(_IncludeInactive, _ExcludeSelf);
+        }
+        
+        /// <summary>
+        /// Searches for the given <see cref="Component"/>s in the children of this <see cref="Transform"/>.
+        /// </summary>
+        /// <param name="_Transform">The <see cref="Transform"/> to search under.</param>
+        /// <param name="_IncludeInactive">Whether to include inactive children in the search.</param>
+        /// <param name="_ExcludeSelf">Set to <c>true</c> to only search for the <see cref="Component"/> in the children and not on this <see cref="Transform"/>.</param>
+        /// <typeparam name="T">Must be of <see cref="Type"/> <see cref="Component"/>.</typeparam>
+        /// <returns>The <see cref="Component"/>s of the given <see cref="Type"/>, or <c>null</c> if none could be found.</returns>
+        public static IEnumerable<T> GetComponentsInChildren<T>(this Transform _Transform, bool _IncludeInactive, bool _ExcludeSelf) where T : Component
+        {
+            return _Transform.gameObject.GetComponentsInChildren<T>(_IncludeInactive, _ExcludeSelf);
         }
         
         /// <summary>
