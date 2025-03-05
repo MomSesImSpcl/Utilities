cfd3f62585cf3c6f67c4f573aa9584f992a46163
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-05 01:26:49 +0100

    Update IListExtensions.cs

--- Assets/Scripts/MomSesImSpcl/Extensions/IListExtensions.cs
+++ Assets/Scripts/MomSesImSpcl/Extensions/IListExtensions.cs
@@ -1,5 +1,10 @@
+#nullable enable
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 using Random = UnityEngine.Random;
 
 namespace MomSesImSpcl.Extensions
@@ -37,6 +42,65 @@ namespace MomSesImSpcl.Extensions
         }
         
         /// <summary>
+        /// Returns random elements from the given <see cref="IList{T}"/>.
+        /// </summary>
+        /// <param name="_IList">The <see cref="IList{T}"/> to select random elements from.</param>
+        /// <param name="_Amount">The number of elements to return.</param>
+        /// <param name="_CanContainDuplicates">
+        /// Set to <c>false</c> to only return unique elements. <br/>
+        /// <b>The given <see cref="IList{T}"/> must contain at least as many elements as the requested amount.</b>
+        /// </param>
+        /// <typeparam name="T">The <see cref="Type"/> of the elements in the <see cref="IList{T}"/>.</typeparam>
+        /// <returns>Randomly selected elements from the enumerable.</returns>
+        /// <exception cref="ArgumentException">Thrown if the enumerable does not contain enough unique elements.</exception>
+        public static IEnumerable<T> GetRandom<T>(this IList<T> _IList, uint _Amount, bool _CanContainDuplicates)
+        {
+            var _random = new System.Random();
+            
+            switch (_CanContainDuplicates)
+            {
+                case false when _IList.Count < _Amount:
+                    throw new ArgumentException($"The enumerable does not contain enough unique elements. Required: {_Amount}, Available: {_IList.Count}");
+                case true:
+                {
+                    // ReSharper disable once InconsistentNaming
+                    for (var i = 0; i < _Amount; i++)
+                    {
+                        yield return _IList[_random.Next(_IList.Count)];
+                    }
+            
+                    break;
+                }
+                default:
+                {
+                    _IList.Shuffle();
+                    
+                    // ReSharper disable once InconsistentNaming
+                    for (var i = 0; i < _Amount; i++)
+                    {
+                        yield return _IList[i];
+                    }
+            
+                    break;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Selects a random subset of elements from the given <see cref="IList{T}"/>.
+        /// </summary>
+        /// <param name="_IList">The <see cref="IList{T}"/> to select from</param>
+        /// <param name="_MaxAmount">The maximum number of elements to select. If 0 or not specified, selects up to the total length of the collection</param>
+        /// <typeparam name="T">The <see cref="Type"/> of elements in the collection</typeparam>
+        /// <returns>A random subset of elements from the original collection</returns>
+        public static IEnumerable<T> GetRandomAmount<T>(this IList<T> _IList, int _MaxAmount = 0)
+        {
+            var _take = Random.Range(1, _MaxAmount > 0 ? _MaxAmount : _IList.Count);
+            
+            return _IList.OrderBy(_ => Guid.NewGuid()).Take(_take);
+        }
+        
+        /// <summary>
         /// Moves an item from one index to another within the list.
         /// </summary>
         /// <param name="_List">The list containing the item to be moved.</param>
@@ -64,6 +128,271 @@ namespace MomSesImSpcl.Extensions
             _List.Insert(_NewIndex, _Item);
         }
 
+        /// <summary>
+        /// Converts an <see cref="IList{T}"/> to a formatted JSON string with specified indentations and selected properties.
+        /// </summary>
+        /// <param name="_IList">The enumerable collection to convert to JSON.</param>
+        /// <param name="_Indentations">The number of spaces to use for indentation.</param>
+        /// <param name="_Entries">Expressions specifying the properties to include in the JSON output.</param>
+        /// <typeparam name="T">The <see cref="Type"/> of the elements in the collection.</typeparam>
+        /// <returns>A JSON string representing the collection with formatted entries and specified indentations.</returns>
+        public static string PrettyJson<T>(this IList<T> _IList, int _Indentations, params Expression<Func<T, object?>>[] _Entries)
+        {
+            var _entries = new List<Expression<Func<T, object?>>>(_Entries);
+            var _lengths = new int[_entries.Count];
+            var _output = new KeyValuePair<string, object?>[_IList.Count][];
+            
+            // ReSharper disable once InconsistentNaming
+            for (var i = 0; i < _IList.Count; i++)
+            {
+                _output[i] = new KeyValuePair<string, object?>[_entries.Count];
+
+                // ReSharper disable once InconsistentNaming
+                for (var j = 0; j < _entries.Count; j++)
+                {
+                    MemberExpression? _memberExpression;
+
+                    if (_entries[j].Body is UnaryExpression { Operand: MemberExpression } _unaryExpression)
+                    {
+                        // For boxed value types. (i.e., bool)
+                        _memberExpression = _unaryExpression.Operand as MemberExpression;
+                    }
+                    else
+                    {
+                        // For reference types or direct member expressions.
+                        _memberExpression = _entries[j].Body as MemberExpression;
+                    }
+                    
+                    var _name = $"\"{_memberExpression!.Member.Name}\": ";
+                    // ReSharper disable once RedundantAssignment
+                    var _value = string.Empty;
+                    var _lastElement = j != _entries.Count - 1 ? ", " : string.Empty;
+                    
+                    var _object = _entries[j].Compile().Invoke(_IList[i]);
+                    // ReSharper disable once ConvertIfStatementToConditionalTernaryExpression
+                    if (_object is IEnumerable _enumerable and not string)
+                    {
+                        _value = $"[{string.Join(", ", from object? _entry in _enumerable select $"\"{_entry}\"")}]{_lastElement}";
+                    }
+                    else
+                    {
+                        _value = $"{(_object != null ? $"\"{_object}\"" : "null")}{_lastElement}";
+                    }
+                    
+                    var _columnLength = _name.Length + _value.Length;
+                    
+                    _output[i][j] = new KeyValuePair<string, object?>(_name, _value);
+
+                    if (_lengths[j] < _columnLength)
+                    {
+                        _lengths[j] = _columnLength;
+                    }
+                }
+            }
+            
+            var _message = string.Empty;
+
+            // ReSharper disable once InconsistentNaming
+            for (var i = 0; i < _output.Length; i++)
+            {
+                _message += string.Empty.PadLeft(_Indentations) + "{ ";
+                
+                // ReSharper disable once InconsistentNaming
+                for (var j = 0; j < _output[i].Length; j++)
+                {
+                    var _name = _output[i][j].Key;
+                    var _value = _output[i][j].Value;
+                    var _length = _lengths[j];
+                    
+                    var _string = $"{_name}{_value}";
+                    
+                    _message += _string.PadRight(_length);
+                }
+
+                _message += " }" + (i != _output.Length - 1 ? ",\n" : string.Empty);
+            }
+            
+            return _message;
+        }
+        
+        /// <summary>
+        /// Prints the elements of the enumerable in a formatted, human-readable string.
+        /// </summary>
+        /// <param name="_IList">The <see cref="IList{T}"/> to format and print.</param>
+        /// <param name="_Title">An optional title to use for the formatted output.</param>
+        /// <param name="_Entries">The properties or fields of the elements to include in the formatted output.</param>
+        /// <typeparam name="T">The type of elements in the enumerable.</typeparam>
+        /// <returns>A formatted string representing the elements of the enumerable.</returns>
+        public static string PrintPretty<T>(this IList<T> _IList, Expression<Func<T, object?>>? _Title = null, params Expression<Func<T, object?>>[] _Entries)
+        {
+            return PrintPretty(_IList, true, true, _Title, _Entries);
+        }
+
+        /// <summary>
+        /// Generates a formatted string representing the provided collection, with options for including a title and specific entries.
+        /// </summary>
+        /// <param name="_IList">The <see cref="IList{T}"/> to be formatted.</param>
+        /// <param name="_PrintName">A boolean flag indicating if the property names should be included in the output.</param>
+        /// <param name="_Title">An optional expression defining the title for the formatted output.</param>
+        /// <param name="_Entries">Expressions defining the entries to be included in the formatted output.</param>
+        /// <typeparam name="T">The type of elements in the collection.</typeparam>
+        /// <returns>A string representing the formatted output of the collection.</returns>
+        public static string PrintPretty<T>(this IList<T> _IList, bool _PrintName, Expression<Func<T, object?>>? _Title = null, params Expression<Func<T, object?>>[] _Entries)
+        {
+            return PrintPretty(_IList, _PrintName, true, _Title, _Entries);
+        }
+
+        /// <summary>
+        /// Formats the elements of the specified collection into a readable string representation.
+        /// </summary>
+        /// <param name="_IList">The <see cref="IList{T}"/> containing the data to format.</param>
+        /// <param name="_PrintName">A boolean flag indicating if the property names should be included in the output.</param>
+        /// <param name="_AddNewLineAtEnd">A boolean flag indicating if a newline should be added at the end of the output.</param>
+        /// <param name="_Title">An expression that selects the title property from the collection's elements.</param>
+        /// <param name="_Entries">An array of expressions that select properties from the collection's elements to include in the output.</param>
+        /// <typeparam name="T">The type of the elements in the collection.</typeparam>
+        /// <returns>A formatted string representation of the collection elements.</returns>
+        public static string PrintPretty<T>(this IList<T> _IList, bool _PrintName = true, bool _AddNewLineAtEnd = true, Expression<Func<T, object?>>? _Title = null, params Expression<Func<T, object?>>[] _Entries)
+        {
+            var _entries = new List<Expression<Func<T, object?>>>(_Entries);
+
+            var _colon = string.Empty;
+            var _dash = string.Empty;
+            var _title = string.Empty;
+            var _additionalTitleCharacters = 1;
+            var _additionalColumnCharacters = 0;
+            
+            if (_Title != null)
+            {
+                _entries.Insert(0, _Title);
+                _title = "[{0}]";
+            }
+            
+            var _lengths = new int[_entries.Count];
+            var _output = new KeyValuePair<string, object?>[_IList.Count][];
+            
+            _additionalTitleCharacters += Regex.Replace(_title, "[{]\\d+[}]", string.Empty).Length;
+            
+            if (_PrintName)
+            {
+                _colon = ": ";
+                
+                _additionalColumnCharacters += _colon.Length;   
+            }
+            // ReSharper disable once RedundantIfElseBlock
+            else
+            {
+                if (_Title == null)
+                {
+                    _dash = "-";
+                    
+                    _additionalColumnCharacters += _dash.Length;
+                }
+            }
+            
+            // ReSharper disable once InconsistentNaming
+            for (var i = 0; i < _IList.Count; i++)
+            {
+                _output[i] = new KeyValuePair<string, object?>[_entries.Count];
+
+                // ReSharper disable once InconsistentNaming
+                for (var j = 0; j < _entries.Count; j++)
+                {
+                    var _name = string.Empty;
+                    
+                    if (_PrintName)
+                    {
+                        MemberExpression? _memberExpression;
+
+                        if (_entries[j].Body is UnaryExpression { Operand: MemberExpression } _unaryExpression)
+                        {
+                            // For boxed value types. (i.e., bool)
+                            _memberExpression = _unaryExpression.Operand as MemberExpression;
+                        }
+                        else
+                        {
+                            // For reference types or direct member expressions.
+                            _memberExpression = _entries[j].Body as MemberExpression;
+                        }   
+                        
+                        _name = _memberExpression!.Member.Name;
+                    }
+                    
+                    // ReSharper disable once RedundantAssignment
+                    var _value = string.Empty;
+                    var _object = _entries[j].Compile().Invoke(_IList[i]);
+                    if (_object is IEnumerable _enumerable and not string)
+                    {
+                        _value = $"{string.Join(", ", from object? _entry in _enumerable select _entry)}";
+                    }
+                    else
+                    {
+                        _value = _object != null ? _object.ToString() : "null";
+                    }
+                    
+                    var _columnLength = _name.Length + (_value?.Length ?? 0);
+
+                    var _isTitle = _Title != null && j == 0;
+                    if (_isTitle)
+                    {
+                        _columnLength += _additionalTitleCharacters;
+                    }
+                    else
+                    {
+                        _columnLength += _additionalColumnCharacters;
+                    }
+                    
+                    _output[i][j] = new KeyValuePair<string, object?>(_name, _value);
+
+                    if (_lengths[j] < _columnLength)
+                    {
+                        _lengths[j] = _columnLength;
+                    }
+                }
+            }
+            
+            var _message = string.Empty;
+            
+            foreach (var _keyValuePairs in _output)
+            {
+                // ReSharper disable once InconsistentNaming
+                for (var i = 0; i < _keyValuePairs.Length; i++)
+                {
+                    var _name = _keyValuePairs[i].Key;
+                    var _value = _keyValuePairs[i].Value;
+                    var _length = _lengths[i];
+                    
+                    string _string;
+                    
+                    var _isTitle = _Title != null && i == 0;
+                    if (_isTitle)
+                    {
+                        _string = string.Format($"{_title} ", _value);
+                        _length -= _name.Length;
+                    }
+                    else
+                    {
+                        _string = i == 0 ? $"{_dash}{_name}{_colon}{_value}" : $"{_name}{_colon}{_value}";
+                    }
+                    
+                    _message += _string.PadRight(_length);
+                    
+                    var _isNotLast = i != _keyValuePairs.Length - 1;
+                    if (!_isTitle && _isNotLast)
+                    {
+                        _message += " | ";
+                    }
+                }
+
+                if (_AddNewLineAtEnd)
+                {
+                    _message += "\n";
+                }
+            }
+
+            return _message;
+        }
+        
         /// <summary>
         /// Removes the first element in this <see cref="IList{T}"/> that matches the given <c>_Condition</c>. <br/>
         /// <i>If no element inside the <see cref="IList{T}"/> matches the given <c>_Condition</c>, nothing will be removed.</i>
