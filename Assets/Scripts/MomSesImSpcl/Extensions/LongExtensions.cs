9f043deead6119a036067f169ec9a281231bb141
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-08 12:11:22 +0100

    Update LongExtensions.cs

--- Assets/Scripts/MomSesImSpcl/Extensions/LongExtensions.cs
+++ Assets/Scripts/MomSesImSpcl/Extensions/LongExtensions.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using MomSesImSpcl.Utilities;
 
 namespace MomSesImSpcl.Extensions
 {
@@ -8,6 +9,16 @@ namespace MomSesImSpcl.Extensions
     public static class LongExtensions
     {
         #region Methods
+        /// <summary>
+        /// Creates a new <see cref="NumericEnumerator"/> to iterate over a sequence of <see cref="long"/>, starting at <c>0</c> and ending at <c>_End</c>.
+        /// </summary>
+        /// <param name="_End"><see cref="NumericEnumerator.end"/>.</param>
+        /// <returns>A new <see cref="NumericEnumerator"/>.</returns>
+        public static NumericEnumerator GetEnumerator(this long _End)
+        {
+            return new NumericEnumerator(_End);
+        }
+        
         /// <summary>
         /// Determines whether the specified long integer has a sign (is negative).
         /// </summary>
