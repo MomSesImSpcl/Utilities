04589127adf23b379f33e8f0f96abf9ea96bdf4e
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-08 12:11:37 +0100

    Update ShortExtensions.cs

--- Assets/Scripts/MomSesImSpcl/Extensions/ShortExtensions.cs
+++ Assets/Scripts/MomSesImSpcl/Extensions/ShortExtensions.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using MomSesImSpcl.Utilities;
 
 namespace MomSesImSpcl.Extensions
 {
@@ -8,6 +9,16 @@ namespace MomSesImSpcl.Extensions
     public static class ShortExtensions
     {
         #region Methods
+        /// <summary>
+        /// Creates a new <see cref="NumericEnumerator"/> to iterate over a sequence of <see cref="short"/>, starting at <c>0</c> and ending at <c>_End</c>.
+        /// </summary>
+        /// <param name="_End"><see cref="NumericEnumerator.end"/>.</param>
+        /// <returns>A new <see cref="NumericEnumerator"/>.</returns>
+        public static NumericEnumerator GetEnumerator(this short _End)
+        {
+            return new NumericEnumerator(_End);
+        }
+        
         /// <summary>
         /// Determines whether the specified short integer has a negative sign.
         /// </summary>
