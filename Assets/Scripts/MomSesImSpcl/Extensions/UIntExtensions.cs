f90e630355b78254090dddaebf63af1bed96e11f
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-08 12:12:38 +0100

    Add files via upload

---
+++ Assets/Scripts/MomSesImSpcl/Extensions/UIntExtensions.cs
@@ -0,0 +1,22 @@
+using MomSesImSpcl.Utilities;
+
+namespace MomSesImSpcl.Extensions
+{
+    /// <summary>
+    /// Contains extension methods for <see cref="uint"/>.
+    /// </summary>
+    public static class UIntExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Creates a new <see cref="NumericEnumerator"/> to iterate over a sequence of <see cref="uint"/>, starting at <c>0</c> and ending at <c>_End</c>.
+        /// </summary>
+        /// <param name="_End"><see cref="NumericEnumerator.end"/>.</param>
+        /// <returns>A new <see cref="NumericEnumerator"/>.</returns>
+        public static NumericEnumerator GetEnumerator(this uint _End)
+        {
+            return new NumericEnumerator(_End);
+        }
+        #endregion
+    }
+}
