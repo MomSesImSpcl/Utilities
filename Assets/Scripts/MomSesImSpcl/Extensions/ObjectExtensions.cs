885c17c7e2c3902597daf45509eb85e3e956cc3c
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-09 13:41:39 +0100

    Update ObjectExtensions.cs

--- Assets/Scripts/MomSesImSpcl/Extensions/ObjectExtensions.cs
+++ Assets/Scripts/MomSesImSpcl/Extensions/ObjectExtensions.cs
@@ -1,5 +1,6 @@
 #nullable enable
 using System;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
@@ -245,6 +246,31 @@ namespace MomSesImSpcl.Extensions
         public static MemoryStream ToMemoryStream<T>(this T? _Object, Encoding _Encoding)
         {
             return new MemoryStream(_Encoding.GetBytes(_Object?.ToString()?.OrNull()!));
+        }
+
+        /// <summary>
+        /// Trims the trailing zeros from a <see cref="float"/>, <see cref="double"/> or <see cref="decimal"/>.
+        /// </summary>
+        /// <param name="_Number">The number to trim the zero from.</param>
+        /// <param name="_Format">The <see cref="System.ValueType.ToString"/> formatting.</param>
+        /// <typeparam name="T">Can only be a numeric <see cref="Type"/>, has no effect on integers, only works on floating point numbers.</typeparam>
+        /// <returns>This number as a <see cref="string"/> with all trailing zeros removed.</returns>
+        public static string TrimTrailingZero<T>(this T _Number, string _Format = "F29") where T : unmanaged, IFormattable
+        {
+            return _Number.ToString(_Format, CultureInfo.InvariantCulture).AsSpan().TrimEnd('0').TrimEnd('.').ToString();
+        }
+        
+        /// <summary>
+        /// Trims the trailing zeros from a <see cref="float"/>, <see cref="double"/> or <see cref="decimal"/>.
+        /// </summary>
+        /// <param name="_Number">The number to trim the zero from.</param>
+        /// <param name="_CultureInfo">The <see cref="CultureInfo"/> to format the <see cref="string"/> with.</param>
+        /// <param name="_Format">The <see cref="System.ValueType.ToString"/> formatting.</param>
+        /// <typeparam name="T">Can only be a numeric <see cref="Type"/>, has no effect on integers, only works on floating point numbers.</typeparam>
+        /// <returns>This number as a <see cref="string"/> with all trailing zeros removed.</returns>
+        public static string TrimTrailingZero<T>(this T _Number, CultureInfo _CultureInfo, string _Format = "F29") where T : unmanaged, IFormattable
+        {
+            return _Number.ToString(_Format, _CultureInfo).AsSpan().TrimEnd('0').TrimEnd('.').ToString();
         }
         #endregion
     }
