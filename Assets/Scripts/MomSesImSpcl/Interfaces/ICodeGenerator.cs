f248f2f67aab50aebf40a705f95c53d597ccee59
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-16 18:13:54 +0100

    Update ICodeGenerator.cs

--- Assets/Scripts/MomSesImSpcl/Interfaces/ICodeGenerator.cs
+++ Assets/Scripts/MomSesImSpcl/Interfaces/ICodeGenerator.cs
@@ -14,13 +14,17 @@ namespace MomSesImSpcl.Interfaces
         /// </summary>
         /// <param name="_UsingStatements">
         /// The using statements in the file. <br/>
-        /// Must only contain the method body, not the entire method. <br/>
+        /// <i>Each element represents one line.</i>
+        /// </param>
+        /// <param name="_Fields">
+        /// The fields in the file (can be empty). <br/>
         /// <i>Each element represents one line.</i>
         /// </param>
         /// <returns>
         /// The code that will be compiled and invoked. <br/>
+        /// Must only contain the method body, not the entire method. <br/>
         /// <i>Each element represents one line.</i></returns>
-        public List<string> GetCode(out List<string> _UsingStatements);
+        public List<string> GetCode(out List<string> _UsingStatements, out List<string> _Fields);
         #endregion
     }
 }
