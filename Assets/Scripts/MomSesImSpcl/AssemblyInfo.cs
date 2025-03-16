cb86a19d95fc950675843373285db6f44b862814
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-16 21:51:16 +0100

    Add files via upload

---
+++ Assets/Scripts/MomSesImSpcl/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("MomSesImSpclEditor")]
+
+namespace MomSesImSpcl
+{
+    /// <summary>
+    /// Used to define Assembly level metadata.
+    /// </summary>
+    // ReSharper disable once UnusedType.Global
+    internal static class AssemblyInfo { }
+}
