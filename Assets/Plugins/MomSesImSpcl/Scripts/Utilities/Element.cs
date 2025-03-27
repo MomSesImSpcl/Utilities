35a219d6e9cc90241c858fba312154144099c3b4
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-27 20:44:15 +0100

    Add files via upload

---
+++ Assets/Plugins/MomSesImSpcl/Scripts/Utilities/Element.cs
@@ -0,0 +1,11 @@
+namespace MomSesImSpcl.Utilities
+{
+    /// <summary>
+    /// Represents the first or last element of a collection.
+    /// </summary>
+    public enum Element
+    {
+        First,
+        Last
+    }
+}
