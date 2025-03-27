3c7f9a8ceeb3b9134efd75d92f70e4a8ef556e5d
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-27 20:41:13 +0100

    Add files via upload

---
+++ Assets/Plugins/MomSesImSpcl/Scripts/Utilities/Edge.cs
@@ -0,0 +1,13 @@
+namespace MomSesImSpcl.Utilities
+{
+    /// <summary>
+    /// Defines the edges of all cardinal directions.
+    /// </summary>
+    public enum Edge
+    {
+        Left, 
+        Top, 
+        Right, 
+        Bottom
+    }
+}
