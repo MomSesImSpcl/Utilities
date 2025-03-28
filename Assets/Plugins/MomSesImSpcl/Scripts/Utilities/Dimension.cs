c82a801f0b3defa787019f3b26f76dbd29003912
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-28 03:01:14 +0100

    Add files via upload

---
+++ Assets/Plugins/MomSesImSpcl/Scripts/Utilities/Dimension.cs
@@ -0,0 +1,12 @@
+namespace MomSesImSpcl.Utilities
+{
+    /// <summary>
+    /// Contains <see cref="Width"/>, <see cref="Height"/> and <see cref="Both"/>.
+    /// </summary>
+    internal enum Dimension
+    {
+        Both = 0,
+        Width,
+        Height
+    }
+}
