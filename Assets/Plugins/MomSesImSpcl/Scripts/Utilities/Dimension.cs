e18225c67eaae6f52a789841d02b5a876651b01c
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-28 03:02:44 +0100

    Update Dimension.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Utilities/Dimension.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Utilities/Dimension.cs
@@ -3,7 +3,7 @@ namespace MomSesImSpcl.Utilities
     /// <summary>
     /// Contains <see cref="Width"/>, <see cref="Height"/> and <see cref="Both"/>.
     /// </summary>
-    internal enum Dimension
+    public enum Dimension
     {
         Both = 0,
         Width,
