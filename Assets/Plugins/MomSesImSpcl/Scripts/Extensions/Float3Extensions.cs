3ae18b1d8a2c119755efaf93e0d5100ee4612f58
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-03-30 03:03:12 +0200

    Update Float3Extensions.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Extensions/Float3Extensions.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Extensions/Float3Extensions.cs
@@ -1,4 +1,5 @@
 using Unity.Mathematics;
+using UnityEngine;
 
 namespace MomSesImSpcl.Extensions
 {
@@ -8,6 +9,16 @@ namespace MomSesImSpcl.Extensions
     public static class Float3Extensions
     {
         #region Methods
+        /// <summary>
+        /// Returns this <see cref="float3"/> as a <see cref="Vector3"/>.
+        /// </summary>
+        /// <param name="_Float3">The <see cref="float3"/> to convert.</param>
+        /// <returns>This <see cref="float3"/> as a <see cref="Vector3"/>.</returns>
+        public static Vector3 AsVector3(this float3 _Float3)
+        {
+            return new Vector3(_Float3.x, _Float3.y, _Float3.z);
+        }
+        
         /// <summary>
         /// Normalizes this <see cref="float3"/>.
         /// </summary>
