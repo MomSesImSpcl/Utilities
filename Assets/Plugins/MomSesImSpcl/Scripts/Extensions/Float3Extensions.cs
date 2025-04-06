1b1342b0b999bda7dc9f8b97119b40b334161f96
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-04-06 16:48:27 +0200

    Update Float3Extensions.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Extensions/Float3Extensions.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Extensions/Float3Extensions.cs
@@ -15,7 +15,6 @@ namespace MomSesImSpcl.Extensions
         /// </summary>
         /// <param name="_Float3">The <see cref="float3"/> to convert.</param>
         /// <returns>This <see cref="float3"/> as a <see cref="Vector3"/>.</returns>
-        [BurstCompile]
         public static Vector3 AsVector3(this float3 _Float3)
         {
             return new Vector3(_Float3.x, _Float3.y, _Float3.z);
@@ -26,6 +25,7 @@ namespace MomSesImSpcl.Extensions
         /// </summary>
         /// <param name="_Float3">The <see cref="float3"/> to normalize.</param>
         /// <returns>This <see cref="float3"/> with normalized values.</returns>
+        [BurstCompile]
         public static float3 Normalize(this float3 _Float3)
         {
             return math.normalize(_Float3);
@@ -41,27 +41,37 @@ namespace MomSesImSpcl.Extensions
         /// In this case cache the timestamp when this method is first started again and pass it in.
         /// </param>
         /// <param name="_OscillationSpeed">Controls the speed of the oscillation.</param>
+        /// <param name="_FadeInDuration">Time in seconds it takes to fade into the starting oscillation value.</param>
         /// <param name="_XAmplitude">Controls how far the <see cref="float3.x"/> value can move from its original value.</param>
         /// <param name="_YAmplitude">Controls how far the <see cref="float3.y"/> value can move from its original value.</param>
+        /// <param name="_ZAmplitude">Controls how far the <see cref="float3.z"/> value can move from its original value.</param>
         /// <param name="_InvertDirection">Inverts the direction of the oscillation if <c>true</c>.</param>
         /// <param name="_NoiseMultiplier">Higher values increase the randomness, but also the speed.</param>
         /// <param name="_XFrequency">Controls the oscillation speed on the <see cref="float3.x"/>-axis.</param>
         /// <param name="_YFrequency">Controls the oscillation speed on the <see cref="float3.y"/>-axis.</param>
+        /// <param name="_ZFrequency">Controls the oscillation speed on the <see cref="float3.z"/>-axis.</param>
         /// <returns>This <see cref="Vector2"/> with the applied oscillation.</returns>
         [BurstCompile]
-        public static float3 Oscillate(this float3 _Float3, float _RealtimeSinceStartup, float _StartTime, float _OscillationSpeed, float _XAmplitude, float _YAmplitude, bool _InvertDirection = false, float _NoiseMultiplier = .5f, float _XFrequency = 1.3f, float _YFrequency = 1.7f)
+        public static float3 Oscillate(this float3 _Float3, float _RealtimeSinceStartup, float _StartTime, float _OscillationSpeed, float _FadeInDuration, float _XAmplitude, float _YAmplitude, float _ZAmplitude, bool _InvertDirection = false, float _NoiseMultiplier = .5f, float _XFrequency = 1.75f, float _YFrequency = 1.25f, float _ZFrequency = .5f)
         {
-            var _time = (_RealtimeSinceStartup - _StartTime) * _OscillationSpeed;
+            var _timeSinceStart = _RealtimeSinceStartup - _StartTime;
+            var _fadeInMultiplier = math.min(1f, _timeSinceStart / _FadeInDuration);
+            
+            var _time = _timeSinceStart * _OscillationSpeed;
             var _sinX = math.sin(_time * _XFrequency);
             var _sinY = math.sin(_time * _YFrequency);
+            var _sinZ = math.sin(_time * _ZFrequency);
             var _noise = _time * _NoiseMultiplier;
-            var _noiseX = noise.snoise(new float2(_noise, 0f)) * 2 - 1;
-            var _noiseY = noise.snoise(new float2(0f, _noise)) * 2 - 1;
+            var _noiseX = noise.snoise(new float3(_noise, 0f, 0f)) * 2 - 1;
+            var _noiseY = noise.snoise(new float3(0f, _noise, 0f)) * 2 - 1;
+            var _noiseZ = noise.snoise(new float3(0f, 0f, _noise)) * 2 - 1;
             var _direction = _InvertDirection.Reverse().AsSignedInt();
-            var _x = _Float3.x + (_sinX + _noiseX) * (_XAmplitude * .5f) * _direction;
-            var _y = _Float3.y + (_sinY + _noiseY) * (_YAmplitude * .5f) * _direction;
-
-            return new float3(_x, _y, _Float3.z);
+            
+            var _x = _Float3.x + (_sinX + _noiseX) * (_XAmplitude * .5f) * _direction * _fadeInMultiplier;
+            var _y = _Float3.y + (_sinY + _noiseY) * (_YAmplitude * .5f) * _direction * _fadeInMultiplier;
+            var _z = _Float3.z + (_sinZ + _noiseZ) * (_ZAmplitude * .5f) * _direction * _fadeInMultiplier;
+            
+            return new float3(_x, _y, _z);
         }
 
         /// <summary>
