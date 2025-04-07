e6a401055eadad063a0885b46db140c66562994c
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-04-07 14:23:26 +0200

    Update CameraExtensions.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Extensions/CameraExtensions.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Extensions/CameraExtensions.cs
@@ -227,7 +227,7 @@ namespace MomSesImSpcl.Extensions
         {
             var _screenPosition = _Camera.WorldToScreenPoint(_WorldPosition);
             var _direction = Input.mousePosition - _screenPosition;
-            var _mouseAngle = math.atan2(_direction.y, _direction.x) * math.TODEGREES;
+            var _mouseAngle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
 
             return _mouseAngle;
         }
