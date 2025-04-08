f972e033a467bddc703db401337dc2abfc08c41a
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-04-09 01:43:42 +0200

    Update SpanExtension.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Extensions/SpanExtension.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Extensions/SpanExtension.cs
@@ -9,6 +9,46 @@ namespace MomSesImSpcl.Extensions
     {
         #region Methods
         /// <summary>
+        /// Searches for an element that matches the conditions defined by the specified predicate, and returns the zero-based index of the first occurrence.
+        /// </summary>
+        /// <param name="_Span">The <see cref="Span{T}"/> to search on.</param>
+        /// <param name="_Item">The item that is being searched for.</param>
+        /// <param name="_Match">The condition to search for.</param>
+        /// <typeparam name="T">The <see cref="Type"/> of the collection.</typeparam>
+        /// <typeparam name="U">The <see cref="Type"/> of the condition to search for.</typeparam>
+        /// <returns>The zero-based index of the first occurrence of an element that matches the conditions defined by match, if found; otherwise, -1.</returns>
+        public static int FindIndex<T,U>(this Span<T> _Span, U _Item, Func<T,U,bool> _Match)
+        {
+            // ReSharper disable once InconsistentNaming
+            for (var i = 0; i < _Span.Length; i++)
+            {
+                if (_Match(_Span[i], _Item))
+                {
+                    return i;
+                }
+            }
+            
+            return -1;
+        }
+
+
+        public static T FirstOrDefault<T,U>(this Span<T> _Span, U _Item, Func<T,U,bool> _Match)
+        {
+            // ReSharper disable once InconsistentNaming
+            // ReSharper disable once ForCanBeConvertedToForeach
+            for (var i = 0; i < _Span.Length; i++)
+            {
+                if (_Match(_Span[i], _Item))
+                {
+                    return _Span[i];
+                }
+            }
+
+            return default;
+        }
+        
+        
+        /// <summary>
         /// Returns a random element from this <see cref="Span{T}"/>.
         /// </summary>
         /// <param name="_Span">The <see cref="Span{T}"/> to get a random element from.</param>
