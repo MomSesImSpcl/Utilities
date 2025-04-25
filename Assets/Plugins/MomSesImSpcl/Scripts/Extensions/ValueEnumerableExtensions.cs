53d98a9a665ec57aeb01b111d2c4348741f87c28
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-04-25 19:13:48 +0200

    Update ValueEnumerableExtensions.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Extensions/ValueEnumerableExtensions.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Extensions/ValueEnumerableExtensions.cs
@@ -15,13 +15,14 @@ namespace MomSesImSpcl.Extensions
         /// </summary>
         /// <param name="_ValueEnumerable"><see cref="ValueEnumerable{TEnumerator,T}"/>.</param>
         /// <param name="_Slice">A <see cref="Span{T}"/> that has exactly the same <see cref="Array.Length"/> as the original <see cref="ValueEnumerable{TEnumerator,T}"/>.</param>
+        /// <param name="_ClearOnDispose"><see cref="ArrayPoolSlice{T}.clearOnDispose"/>.</param>
         /// <typeparam name="E">The <see cref="Type"/> of the <see cref="ValueEnumerator{TEnumerator,T}"/>.</typeparam>
         /// <typeparam name="A">The <see cref="Type"/> of the <see cref="Array"/>.</typeparam>
         /// <returns><see cref="ArrayPoolSlice{T}"/>.</returns>
-        public static ArrayPoolSlice<A> ToArrayPoolSlice<E,A>(this ValueEnumerable<E,A> _ValueEnumerable, out Span<A> _Slice) where E : struct, IValueEnumerator<A>
+        public static ArrayPoolSlice<A> ToArrayPoolSlice<E,A>(this ValueEnumerable<E,A> _ValueEnumerable, out Span<A> _Slice, bool _ClearOnDispose = false) where E : struct, IValueEnumerator<A>
         {
             var _arrayTuple = _ValueEnumerable.ToArrayPool();
-            var _arrayPoolSlice = new ArrayPoolSlice<A>(_arrayTuple);
+            var _arrayPoolSlice = new ArrayPoolSlice<A>(_arrayTuple, _ClearOnDispose);
             
             _Slice = _arrayPoolSlice.GetSlice();
             return _arrayPoolSlice;
@@ -31,13 +32,14 @@ namespace MomSesImSpcl.Extensions
         /// Materializes the <see cref="ValueEnumerable{TEnumerator,T}"/> into an <see cref="ArrayPoolSlice{T}"/>.
         /// </summary>
         /// <param name="_ValueEnumerable"><see cref="ValueEnumerable{TEnumerator,T}"/>.</param>
+        /// <param name="_ClearOnDispose"><see cref="ArrayPoolSlice{T}.clearOnDispose"/>.</param> 
         /// <typeparam name="E">The <see cref="Type"/> of the <see cref="ValueEnumerator{TEnumerator,T}"/>.</typeparam>
         /// <typeparam name="A">The <see cref="Type"/> of the <see cref="Array"/>.</typeparam>
         /// <returns><see cref="ArrayPoolSlice{T}"/>.</returns>
-        public static ArrayPoolSlice<A> ToArrayPoolSlice<E,A>(this ValueEnumerable<E,A> _ValueEnumerable) where E : struct, IValueEnumerator<A>
+        public static ArrayPoolSlice<A> ToArrayPoolSlice<E,A>(this ValueEnumerable<E,A> _ValueEnumerable, bool _ClearOnDispose = false) where E : struct, IValueEnumerator<A>
         {
             var _arrayTuple = _ValueEnumerable.ToArrayPool();
-            return new ArrayPoolSlice<A>(_arrayTuple);
+            return new ArrayPoolSlice<A>(_arrayTuple, _ClearOnDispose);
         }
         #endregion
     }
