a278e9ee80f133ff3bd7d5737b82c6e5a3dbedd7
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-03 00:41:51 +0200

    Update EventReferenceExtensions.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Extensions/EventReferenceExtensions.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Extensions/EventReferenceExtensions.cs
@@ -28,11 +28,40 @@ namespace MomSesImSpcl.Extensions
         /// Plays the audio for the given <see cref="EventReference"/> and set the value for the parameter with the given <see cref="PARAMETER_ID"/>.
         /// </summary>
         /// <param name="_EventReference">The <see cref="EventReference"/> of the audio event to play.</param>
+        /// <param name="_StopMode">Determines how the previous event should be stopped.</param>
+        public static void PlayOneShot(this EventReference _EventReference, STOP_MODE _StopMode)
+        {
+            var _eventInstance = RuntimeManager.CreateInstance(_EventReference);
+            _eventInstance.stop(_StopMode);
+            _eventInstance.start();
+            _eventInstance.release();
+        }
+        
+        /// <summary>
+        /// Plays the audio for the given <see cref="EventReference"/> and set the value for the parameter with the given <see cref="PARAMETER_ID"/>.
+        /// </summary>
+        /// <param name="_EventReference">The <see cref="EventReference"/> of the audio event to play.</param>
         /// <param name="_ParameterId">The id of the parameter in FMOD.</param>
         /// <param name="_ParameterValue">The value to set the parameter to.</param>
         public static void PlayOneShot(this EventReference _EventReference, PARAMETER_ID _ParameterId, float _ParameterValue)
         {
             var _eventInstance = RuntimeManager.CreateInstance(_EventReference);
+            _eventInstance.setParameterByID(_ParameterId, _ParameterValue);
+            _eventInstance.start();
+            _eventInstance.release();
+        }
+        
+        /// <summary>
+        /// Plays the audio for the given <see cref="EventReference"/> and set the value for the parameter with the given <see cref="PARAMETER_ID"/>.
+        /// </summary>
+        /// <param name="_EventReference">The <see cref="EventReference"/> of the audio event to play.</param>
+        /// <param name="_StopMode">Determines how the previous event should be stopped.</param>
+        /// <param name="_ParameterId">The id of the parameter in FMOD.</param>
+        /// <param name="_ParameterValue">The value to set the parameter to.</param>
+        public static void PlayOneShot(this EventReference _EventReference, STOP_MODE _StopMode, PARAMETER_ID _ParameterId, float _ParameterValue)
+        {
+            var _eventInstance = RuntimeManager.CreateInstance(_EventReference);
+            _eventInstance.stop(_StopMode);
             _eventInstance.setParameterByID(_ParameterId, _ParameterValue);
             _eventInstance.start();
             _eventInstance.release();
