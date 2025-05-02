82b84ad42abf243608cc80a93c91edbcf02b64c7
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-02 23:09:10 +0200

    Add files via upload

---
+++ Assets/Plugins/MomSesImSpcl/Scripts/Extensions/EventInstanceExtensions.cs
@@ -0,0 +1,28 @@
+#if FMOD
+using FMOD.Studio;
+
+namespace MomSesImSpcl.Extensions
+{
+    /// <summary>
+    /// Contains extension methods for <see cref="EventInstance"/>.
+    /// </summary>
+    public static class EventInstanceExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Returns the <see cref="PARAMETER_ID"/> for the given name.
+        /// </summary>
+        /// <param name="_EventInstance">The <see cref="EventInstance"/> to get the parameter from.</param>
+        /// <param name="_ParameterName">The name of the parameter to get the <see cref="PARAMETER_ID"/> of.</param>
+        /// <returns>The <see cref="PARAMETER_ID"/> for the given name.</returns>
+        public static PARAMETER_ID GetParameterId(this EventInstance _EventInstance, string _ParameterName)
+        {
+            _EventInstance.getDescription(out var _eventDescription);
+            _eventDescription.getParameterDescriptionByName(_ParameterName, out var _parameterDescription);
+
+            return _parameterDescription.id;
+        }
+        #endregion
+    }
+}
+#endif
