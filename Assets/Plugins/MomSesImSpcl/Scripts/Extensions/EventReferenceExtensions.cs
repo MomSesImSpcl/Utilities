26912aadf32978fda58366d1d11a61d9026f3609
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-03 18:09:49 +0200

    Update EventReferenceExtensions.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Extensions/EventReferenceExtensions.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Extensions/EventReferenceExtensions.cs
@@ -28,13 +28,27 @@ namespace MomSesImSpcl.Extensions
         /// Plays the audio for the given <see cref="EventReference"/> and set the value for the parameter with the given <see cref="PARAMETER_ID"/>.
         /// </summary>
         /// <param name="_EventReference">The <see cref="EventReference"/> of the audio event to play.</param>
+        /// <param name="_EventInstance">The <see cref="EventInstance"/> to stop before creating the new one.</param>
         /// <param name="_StopMode">Determines how the previous event should be stopped.</param>
-        public static void PlayOneShot(this EventReference _EventReference, STOP_MODE _StopMode)
+        /// <param name="_Release">Set to <c>true</c> to immediately release the <see cref="EventInstance"/> after starting it.</param>
+        /// <returns>The <see cref="EventInstance"/> that was created to play the sound.</returns>
+        public static EventInstance PlayOneShot(this EventReference _EventReference, EventInstance _EventInstance, STOP_MODE _StopMode, bool _Release)
         {
+            if (_EventInstance.isValid())
+            {
+                _EventInstance.stop(_StopMode);
+                _EventInstance.release();
+            }
+            
             var _eventInstance = RuntimeManager.CreateInstance(_EventReference);
-            _eventInstance.stop(_StopMode);
             _eventInstance.start();
-            _eventInstance.release();
+            
+            if (_Release)
+            {
+                _eventInstance.release();
+            }
+            
+            return _eventInstance;
         }
         
         /// <summary>
@@ -43,28 +57,45 @@ namespace MomSesImSpcl.Extensions
         /// <param name="_EventReference">The <see cref="EventReference"/> of the audio event to play.</param>
         /// <param name="_ParameterId">The id of the parameter in FMOD.</param>
         /// <param name="_ParameterValue">The value to set the parameter to.</param>
-        public static void PlayOneShot(this EventReference _EventReference, PARAMETER_ID _ParameterId, float _ParameterValue)
+        /// <returns>The <see cref="EventInstance"/> that was created to play the sound.</returns>
+        public static EventInstance PlayOneShot(this EventReference _EventReference, PARAMETER_ID _ParameterId, float _ParameterValue)
         {
             var _eventInstance = RuntimeManager.CreateInstance(_EventReference);
             _eventInstance.setParameterByID(_ParameterId, _ParameterValue);
             _eventInstance.start();
             _eventInstance.release();
+
+            return _eventInstance;
         }
         
         /// <summary>
         /// Plays the audio for the given <see cref="EventReference"/> and set the value for the parameter with the given <see cref="PARAMETER_ID"/>.
         /// </summary>
         /// <param name="_EventReference">The <see cref="EventReference"/> of the audio event to play.</param>
+        /// <param name="_EventInstance">The <see cref="EventInstance"/> to stop before creating the new one.</param>
         /// <param name="_StopMode">Determines how the previous event should be stopped.</param>
         /// <param name="_ParameterId">The id of the parameter in FMOD.</param>
         /// <param name="_ParameterValue">The value to set the parameter to.</param>
-        public static void PlayOneShot(this EventReference _EventReference, STOP_MODE _StopMode, PARAMETER_ID _ParameterId, float _ParameterValue)
+        /// <param name="_Release">Set to <c>true</c> to immediately release the <see cref="EventInstance"/> after starting it.</param>
+        /// <returns>The <see cref="EventInstance"/> that was created to play the sound.</returns>
+        public static EventInstance PlayOneShot(this EventReference _EventReference, EventInstance _EventInstance, STOP_MODE _StopMode, PARAMETER_ID _ParameterId, float _ParameterValue, bool _Release)
         {
+            if (_EventInstance.isValid())
+            {
+                _EventInstance.stop(_StopMode);
+                _EventInstance.release();
+            }
+            
             var _eventInstance = RuntimeManager.CreateInstance(_EventReference);
-            _eventInstance.stop(_StopMode);
             _eventInstance.setParameterByID(_ParameterId, _ParameterValue);
             _eventInstance.start();
-            _eventInstance.release();
+
+            if (_Release)
+            {
+                _eventInstance.release();
+            }
+            
+            return _eventInstance;
         }
         #endregion
     }
