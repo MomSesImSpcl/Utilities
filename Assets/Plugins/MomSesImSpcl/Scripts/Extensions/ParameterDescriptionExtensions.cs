8f8ee14b4c28795966c17e57fcc58d5e22df37b4
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-05 22:41:53 +0200

    Add files via upload

---
+++ Assets/Plugins/MomSesImSpcl/Scripts/Extensions/ParameterDescriptionExtensions.cs
@@ -0,0 +1,22 @@
+using FMOD.Studio;
+
+namespace MomSesImSpcl.Extensions
+{
+    /// <summary>
+    /// Contains extension methods for <see cref="PARAMETER_DESCRIPTION"/>.
+    /// </summary>
+    public static class ParameterDescriptionExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Returns the actual parameter name of this <see cref="PARAMETER_DESCRIPTION"/>.
+        /// </summary>
+        /// <param name="_ParameterDescription">The <see cref="PARAMETER_DESCRIPTION"/> to get the name of.</param>
+        /// <returns>The actual parameter name of this <see cref="PARAMETER_DESCRIPTION"/>.</returns>
+        public static string GetName(this PARAMETER_DESCRIPTION _ParameterDescription)
+        {
+            return _ParameterDescription.name;
+        }
+        #endregion
+    }
+}
