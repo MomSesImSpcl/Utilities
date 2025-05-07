cbba6a97179946009290f59cc9bdf097885e6153
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-07 03:07:48 +0200

    Add files via upload

---
+++ Assets/Plugins/MomSesImSpcl/Scripts/Utilities/LifeCycle.cs
@@ -0,0 +1,15 @@
+namespace MomSesImSpcl.Utilities
+{
+    /// <summary>
+    /// Represents the unity lifecycle methods.
+    /// </summary>
+    public enum LifeCycle
+    {
+        Awake,
+        OnEnable,
+        Start,
+        OnValidate,
+        OnDisable,
+        OnDestroy
+    }
+}
