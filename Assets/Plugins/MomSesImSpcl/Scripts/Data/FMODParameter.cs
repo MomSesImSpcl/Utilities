41170bdf4aab70c9f6c7691b19d45eb85a81ca76
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-07 02:58:48 +0200

    Update FMODParameter.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Data/FMODParameter.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Data/FMODParameter.cs
@@ -13,10 +13,12 @@ namespace MomSesImSpcl.Data
     public struct FMODParameter
     {
         #region Fields
+#if UNITY_EDITOR
         /// <summary>
         /// The name of the parameter in FMOD.
         /// </summary>
         private string parameterName;
+#endif
         /// <summary>
         /// The <see cref="GUID"/> of the <see cref="EventDescription"/> the <see cref="PARAMETER_DESCRIPTION"/> belongs to.
         /// </summary>
@@ -28,10 +30,12 @@ namespace MomSesImSpcl.Data
         #endregion
         
         #region Properties
+#if UNITY_EDITOR
         /// <summary>
         /// <see cref="parameterName"/>.
         /// </summary>
         public string ParameterName => this.parameterName;
+#endif
         /// <summary>
         /// <see cref="eventDescriptionId"/>.
         /// </summary>
@@ -42,6 +46,7 @@ namespace MomSesImSpcl.Data
         public PARAMETER_ID ParameterId => this.parameterId.ParameterId;
         #endregion
         
+#if UNITY_EDITOR
         #region Operators
         /// <summary>
         /// Implicitly returns the <see cref="parameterName"/>.
@@ -50,7 +55,7 @@ namespace MomSesImSpcl.Data
         /// <returns><see cref="parameterName"/>.</returns>
         public static implicit operator string(FMODParameter _FMODParameter) => _FMODParameter.parameterName;
         #endregion
-        
+#endif
         #region Constructors
         /// <summary>
         /// <see cref="FMODParameter"/>.
@@ -61,11 +66,23 @@ namespace MomSesImSpcl.Data
         {
             _EventDescription.getID(out this.eventDescriptionId);
             this.parameterId = new SerializedParameterId(_ParameterDescription.id);
+#if UNITY_EDITOR
             this.parameterName = _ParameterDescription.GetName();
+#endif
         }
         #endregion
         
         #region Methods
+        /// <summary>
+        /// Returns <c>true</c> if this <see cref="FMODParameter"/> has been correctly initialized, otherwise <c>false</c>.
+        /// </summary>
+        /// <returns><c>true</c> if this <see cref="FMODParameter"/> has been correctly initialized, otherwise <c>false</c>.</returns>
+        public bool IsValid()
+        {
+            return !this.eventDescriptionId.IsNull;
+        }
+        
+#if UNITY_EDITOR
         /// <summary>
         /// Returns the <see cref="parameterName"/>.
         /// </summary>
@@ -74,6 +91,7 @@ namespace MomSesImSpcl.Data
         {
             return this.parameterName;
         }
+#endif
         #endregion
     }
 }
