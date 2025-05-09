c6b083807ae7dd81d898f80f258e2fe0bb4b1bc7
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-10 00:46:22 +0200

    Add files via upload

---
+++ Assets/Plugins/MomSesImSpcl/Scripts/Components/EditorMonoBehaviour.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace MomSesImSpcl.Components
+{
+    /// <summary>
+    /// Destroys this <see cref="GameObject"/> in build.
+    /// </summary>
+    public abstract class EditorMonoBehaviour : MonoBehaviour
+    {
+        #region Methods
+        protected virtual void Awake()
+        {
+            if (!Application.isEditor)
+            {
+               Destroy(base.gameObject); 
+            }
+        }
+        #endregion
+    }
+}
