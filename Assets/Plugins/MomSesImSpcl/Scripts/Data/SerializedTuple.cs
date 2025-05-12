6c47c1b1f611e725bdb8728e3c3063f68f14e7fd
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-12 05:16:57 +0200

    Update SerializedTuple.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Data/SerializedTuple.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Data/SerializedTuple.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using MomSesImSpcl.Extensions;
 using UnityEditor;
 using UnityEngine;
@@ -10,7 +11,7 @@ namespace MomSesImSpcl.Data
     /// </summary>
     /// <typeparam name="T">The type of the elements in the tuple.</typeparam>
     [Serializable]
-    public struct SerializedTuple<T>
+    public struct SerializedTuple<T> : IEquatable<SerializedTuple<T>>
     {
         #region Inspector Fields
 #if ODIN_INSPECTOR
@@ -57,6 +58,28 @@ namespace MomSesImSpcl.Data
         /// <param name="_SerializedTuple">The <see cref="SerializedTuple{T}"/> to convert.</param>
         /// <returns>A <see cref="Tuple{T1, T2}"/> with the values of this <see cref="SerializedTuple{T}"/>.</returns>
         public static implicit operator (T, T) (SerializedTuple<T> _SerializedTuple) => (_SerializedTuple.item1, _SerializedTuple.item2);
+
+        /// <summary>
+        /// Compares <see cref="Item1"/> and <see cref="Item2"/> for the given <see cref="SerializedTuple{T}"/> for equality.
+        /// </summary>
+        /// <param name="_SerializedTuple1"><see cref="SerializedTuple{T}"/>.</param>
+        /// <param name="_SerializedTuple2"><see cref="SerializedTuple{T}"/>.</param>
+        /// <returns><c>true</c> if both <see cref="SerializedTuple{T}"/> are equal, otherwise <c>false</c>.</returns>
+        public static bool operator ==(SerializedTuple<T> _SerializedTuple1, SerializedTuple<T> _SerializedTuple2)
+        {
+            return EqualityComparer<T>.Default.Equals(_SerializedTuple1.item1, _SerializedTuple2.item1) && EqualityComparer<T>.Default.Equals(_SerializedTuple1.item2, _SerializedTuple2.item2);
+        }
+        
+        /// <summary>
+        /// Compares <see cref="Item1"/> and <see cref="Item2"/> for the given <see cref="SerializedTuple{T}"/> for inequality.
+        /// </summary>
+        /// <param name="_SerializedTuple1"><see cref="SerializedTuple{T}"/>.</param>
+        /// <param name="_SerializedTuple2"><see cref="SerializedTuple{T}"/>.</param>
+        /// <returns><c>true</c> if both <see cref="SerializedTuple{T}"/> are not equal, otherwise <c>false</c>.</returns>
+        public static bool operator !=(SerializedTuple<T> _SerializedTuple1, SerializedTuple<T> _SerializedTuple2)
+        {
+            return !(_SerializedTuple1 == _SerializedTuple2);
+        }
         #endregion
         
         #region Constructors
@@ -77,6 +100,30 @@ namespace MomSesImSpcl.Data
         #endregion
         
         #region Methods
+        public bool Equals(SerializedTuple<T> _SerializedTuple)
+        {
+            return this == _SerializedTuple;
+        }
+        
+        public override bool Equals(object _Object)
+        {
+            if (_Object is SerializedTuple<T> _serializedTuple)
+            {
+                return this == _serializedTuple;
+            }
+            
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            var _hash1 = this.item1 == null ? 0 : item1.GetHashCode();
+            var _hash2 = this.item2 == null ? 0 : item2.GetHashCode();
+            
+            // Use a hashing strategy that combines both
+            return _hash1 * 397 ^ _hash2;
+        }
+        
         /// <summary>
         /// Deconstructor for <see cref="SerializedTuple{T}"/>.
         /// </summary>
