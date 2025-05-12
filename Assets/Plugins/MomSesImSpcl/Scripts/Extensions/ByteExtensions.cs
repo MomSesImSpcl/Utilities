fab4bef0689f0cd71f03fd959061987b5054195c
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-12 04:30:04 +0200

    Update ByteExtensions.cs

--- Assets/Plugins/MomSesImSpcl/Scripts/Extensions/ByteExtensions.cs
+++ Assets/Plugins/MomSesImSpcl/Scripts/Extensions/ByteExtensions.cs
@@ -10,6 +10,17 @@ namespace MomSesImSpcl.Extensions
     {
         #region Methods
         /// <summary>
+        /// Returns this <see cref="byte"/> as a <see cref="float"/>.
+        /// </summary>
+        /// <param name="_Byte">The <see cref="byte"/> to cast to a <see cref="float"/>.</param>
+        /// <returns>This <see cref="byte"/> cast to a <see cref="float"/>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float AsFloat(this byte _Byte)
+        {
+            return (float)_Byte;
+        }
+        
+        /// <summary>
         /// Creates a new <see cref="NumericEnumerator"/> to iterate over a sequence of <see cref="byte"/>, starting at <c>0</c> and ending at <c>_End</c>.
         /// </summary>
         /// <param name="_End"><see cref="NumericEnumerator.end"/>.</param>
