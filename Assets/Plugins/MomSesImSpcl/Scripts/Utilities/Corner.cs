76965d0b580db1bd540c37bd2f161e285e0cdb57
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-13 11:22:42 +0200

    Add files via upload

---
+++ Assets/Plugins/MomSesImSpcl/Scripts/Utilities/Corner.cs
@@ -0,0 +1,18 @@
+namespace MomSesImSpcl.Utilities
+{
+    /// <summary>
+    /// Defines corners in a rectangle.
+    /// </summary>
+    public enum Corner
+    {
+        UpperLeft,
+        UpperCenter,
+        UpperRight,
+        MiddleLeft,
+        MiddleCenter,
+        MiddleRight,
+        LowerLeft,
+        LowerCenter,
+        LowerRight
+    }
+}
