98f2b8ee8c488fffc656dd6a50911fdb6fc2a45b
MomSesImSpcl <58560741+MomSesImSpcl@users.noreply.github.com>
2025-05-22 09:53:39 +0200

    Add files via upload

---
+++ Assets/Plugins/MomSesImSpcl/Scripts/Components/DevelopmentMonoBehaviour.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace MomSesImSpcl.Components
+{
+    /// <summary>
+    /// Destroys this <see cref="GameObject"/> in non development builds.
+    /// </summary>
+    public class DevelopmentMonoBehaviour : MonoBehaviour
+    {
+        #region Methods
+        protected virtual void Awake()
+        {
+            if (!Application.isEditor && Debug.isDebugBuild)
+            {
+                Destroy(base.gameObject);
+            }
+        }
+        #endregion
+    }
+}
